d6d98006b04fe6bce0b35455ea5f4d609bb5273d
Kurucz István <kurucz.ist63@gmail.com>
2023-02-10 19:54:12 +0100

    new env vars

--- middlewares/file/getFilesMW.js
+++ middlewares/file/getFilesMW.js
@@ -2,7 +2,6 @@
 import { gridFs } from "../../config/mongoose.js";
 
 export default async function (req, res, next) {
-	console.log("gfs in MW:\n", gridFs);
 	try {
 		const files = await gridFs.gfs.files.find().toArray();
 		if (!files.length) return next("No image was found.");

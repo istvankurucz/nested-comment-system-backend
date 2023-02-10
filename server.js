50a11ef44f034eb6d0979c326b1a5ccd5d46a977
Kurucz István <kurucz.ist63@gmail.com>
2023-02-10 19:46:34 +0100

    cors origin added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import * as dotenv from "dotenv";
 
 import connectToDb from "./config/mongoose.js";
 import userRoute from "./routes/user.js";
@@ -7,11 +8,17 @@ import postRoute from "./routes/post.js";
 import commentRoute from "./routes/comment.js";
 import uploadRoute from "./routes/upload.js";
 
+dotenv.config();
+
 const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+	})
+);
 
 // Routing
 app.use("/users", userRoute);

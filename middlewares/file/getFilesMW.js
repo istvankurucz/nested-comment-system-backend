929f97a3a8579fba69a140185167757bc2fce8c8
Kurucz István <kurucz.ist63@gmail.com>
2023-02-10 11:30:12 +0100

    test gridfs

--- middlewares/file/getFilesMW.js
+++ middlewares/file/getFilesMW.js
@@ -1,6 +1,8 @@
 import { gfs } from "../../config/mongoose.js";
 
 export default async function (req, res, next) {
+	console.log("GridFS in getFilesMW:\n", gfs);
+
 	try {
 		const files = await gfs.files.find().toArray();
 		if (!files.length) return next("No image was found.");
